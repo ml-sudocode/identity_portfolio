457702d599ddae846b22b2edea0b98df6ac02907
tomholford <tomholford@users.noreply.github.com>
2022-10-09 05:15:12 -0700

    add additional actions to Actio

--- src/views/actions/ActionsIndex.tsx
+++ src/views/actions/ActionsIndex.tsx
@@ -7,13 +7,15 @@ import { TransactionsTable } from "../transactions/TransactionsTable";
 import { TransactionsSummary } from "../transactions/TransactionsSummary";
 import { Link } from "react-router-dom";
 import AddContactButton from "../contacts/AddContactButton";
+import AddAddressButton from "../addresses/AddAddressButton";
+import AddWalletButton from "../wallets/AddWalletButton";
 
 export default function ActionsIndex() {
-
-
   return <>
     <h1 className='text-2xl'><Link to={'/home'} className='text-gray-500'>Home /</Link> Actions</h1>
     <div className="buttons-container">
+      <AddWalletButton />
+      <AddAddressButton />
       <AddTransactionButton />
       <AddContactButton />
     </div>
